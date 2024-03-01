0a68e1933b333cb913dbc72476c2f613d499cf22
Andre Marroquin <mar22266@uvg.edu.gt>
2024-02-29 20:05:37 -0600

    Feat: added corse

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import {
   getAllPosts,
   insertPosts,
@@ -13,6 +14,7 @@ import {
 } from './log.js'
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
